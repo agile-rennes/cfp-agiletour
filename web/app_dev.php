9796224b08c1c99afc8f6e78fed890b61c7ceaf4
Serge HARDY <serge.hardy@zenika.com>
2017-11-21 08:05:42 +0100

    Dev mode

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,10 +10,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
-) {
+
+$authorizedHosts = ['localhost', 'ancient-scrubland-90466.herokuapp.com'];
+$isAuthorized = in_array($_SERVER['HTTP_HOST'], $authorizedHosts);
+
+if (!$isAuthorized) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
